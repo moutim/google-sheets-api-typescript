31df2c093e356a929aa6baa3daf16cc392e1c808
Vitor Moutim <moutimg@gmail.com>
2024-02-04 01:12:55 -0300

    docs: create swagger documentation

---
+++ src/docs/swagger.config.ts
@@ -0,0 +1,17 @@
+const path = require('path');
+const swaggerConfig = {
+  definition: {
+    openapi: '3.0.1',
+    info: {
+      title: 'API to manipulate Google Sheets.',
+      description: 'This API manipulates a spreadsheet through the Google Sheets API.',
+      version: '1.0',
+    },
+    servers: [
+      { url: 'http://localhost:3001/', description: 'Development environment' },
+    ],
+  },
+  apis: [path.resolve(__dirname, '../routes/sheets.routes.ts')],
+};
+
+module.exports = swaggerConfig;
--- src/routes/sheets.routes.ts
+++ src/routes/sheets.routes.ts
@@ -1,14 +1,132 @@
 import { Router } from 'express';
-const { StatusCodes } = require('http-status-codes');
 import controllers from '../controllers/sheets.controllers';
 import verifyRegister from '../middlewares/createStudent';
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJSONDoc = swaggerJSDoc(require('../docs/swagger.config'));
 
 const sheetsRouter = Router();
+
+sheetsRouter.use('/documentation', swaggerUI.serve, swaggerUI.setup(swaggerJSONDoc));
 
 sheetsRouter.get('/read/students', controllers.getSheetsRow);
 
 sheetsRouter.post('/create/students', verifyRegister, controllers.createStudent);
 
-sheetsRouter.put('/calculate/avarage', controllers.calculateAverage);
+sheetsRouter.put('/calculate/average', controllers.calculateAverage);
+
+/**
+ * @swagger
+ *   /read/students:
+ *     get:
+ *       tags: [Students]
+ *       description: Return data for all students entered in the spreadsheet
+ *       responses:
+ *         200:
+ *           description: Data returned successfully
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: number
+ *                     name:
+ *                       type: string
+ *                     studyArea:
+ *                       type: string
+ *                     absences:
+ *                       type: number
+ *                     test1:
+ *                       type: number
+ *                     test2:
+ *                       type: number
+ *                     test3:
+ *                       type: number
+ *                     status:
+ *                       type: string
+ *                     finalApprovalScore:
+ *                       type: number
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     StudentRequest:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *         absences:
+ *           type: integer
+ *         test1:
+ *           type: integer
+ *         test2:
+ *           type: integer
+ *         test3:
+ *           type: integer
+ *       required:
+ *         - name
+ *         - absences
+ *         - test1
+ *         - test2
+ *         - test3
+ *
+ * /create/students:
+ *   post:
+ *     tags: [Students]
+ *     description: Creates a new student in the spreadsheet.
+ *     consumes:
+ *       - application/json
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/StudentRequest'
+ *     responses:
+ *       201:
+ *         description: Student created successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Message indicating the success of the operation.
+ *       400:
+ *         description: Invalid request. Check the values ​​sent in the request body.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Message indicating invalid values ​​in the request body.
+ */
+
+/**
+ * @swagger
+ *   /calculate/average:
+ *     put:
+ *       tags: [Students]
+ *       description: Calculates the average of student grades in the spreadsheet.
+ *       responses:
+ *         200:
+ *           description: Operation performed successfully.
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   message:
+ *                     type: string
+ *                     description: Message indicating the success of the operation.
+ */
 
 export default sheetsRouter;
